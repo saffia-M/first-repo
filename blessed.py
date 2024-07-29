8f4eb4c668bb91b07e91eebe0d6c7f96f6367131
saffia-M <saffiamohammed@gmail.com>
2024-07-29 09:25:11 -0700

    added elif

--- blessed.py
+++ blessed.py
@@ -1,1 +1,7 @@
-print("i'm blessed")
+blessed=input("are u muslim")
+if blessed=="yes":
+    print(" say alhamduliallah")
+elif blessed=="no":
+    print("read about islam")
+else:
+    print("please enter either yes or no")
